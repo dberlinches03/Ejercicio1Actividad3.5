18aeda2da7b19e4a7f3ca0f2a05c6b7013e4f5db
dberl <dberlinches2003@gmail.com>
2025-12-18 12:34:45 +0100

    Commit inicial

---
+++ src/main/java/org/psp/ServidorTCP.java
@@ -0,0 +1,27 @@
+package org.psp;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTCP {
+    static final int PUERTO = 5000;
+
+    public static void main(String[] args) {
+        try (ServerSocket servidor = new ServerSocket(PUERTO)) {
+            System.out.println("Servidor TCP iniciado en el puerto " + PUERTO);
+
+            while (true) {
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente conectado");
+
+                // Un hilo por cliente
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
